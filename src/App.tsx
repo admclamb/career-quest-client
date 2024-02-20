a71fc4e1e028ad7f318904e149074344ea917171
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-19 23:15:08 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return <h1>TESTING</h1>;
+};
+
+export default App;
---
+++ src/layout/footer/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="text-center p-3 border-t">Website Made with 💙</div>;
+};
+
+export default Footer;
